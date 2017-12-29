0bc605117b52ac6a86fdc7bc35ec171ad094e4ca
mrmrs <hi@mrmrs.cc>
2017-12-29 23:12:54 +0100

    Add template for About page

---
+++ About.jsx
@@ -0,0 +1,64 @@
+---
+avatar: 'https://c8r.imgix.net/87760e0718677237f07e486c/timothy.jpg'
+twitter: mrmrs_
+github: mrmrs
+instagram: mrmrs_
+dribbble: mrmrs
+links:
+  - text: Link 1
+    href: '#0'
+  - text: Link 2
+    href: '#0'
+  - text: Link 3
+    href: '#0'
+  - text: Link 4
+    href: '#0'
+  - text: Link 5
+    href: '#0'
+copyright: '© 2017 You, Inc.'
+image: 'https://c8r.imgix.net/59b19cfb647d327d75bc316c/63.jpg'
+title: About
+subtitle: A brief incomplete history
+text: Started out as a single-celled organism. Currently a multi-celled organism.
+---
+<Container px={3} py={3}>
+  <SiteHeader 
+    src={props.avatar}
+    links={props.links} 
+  />
+</Container>
+<HorizontalRule color='#eee' />
+<PageBG half image={props.image} />
+<Container py={5}>
+  
+  <PageTitle children={props.title} />
+  <PageSubtitle children={props.subtitle} />
+ 
+    <Text children={props.text} />
+</Container>
+<HorizontalRule color='#eee' />
+<footer>
+  <Container pt={5} pb={4} px={3}>
+    <Flex align='center'>
+      <Box w={1/2}>
+        <Text fontSize={1}>
+          I'm currently available for contracts in Q2 of 2018. If you're
+          interested in collaborating on a project drop me a line.
+        </Text>
+        <Text fontSize={0}>© 2017 You, Inc.</Text>
+      </Box>
+      <Flex pt={4} pb={4} justify='center' ml='auto'>
+        {props.twitter && <TwitterIcon href={'https://twitter.com/'+props.twitter} /> }
+        {props.github && <GitHubIcon href={'https://github.com/'+props.github} /> }
+        {props.facebook && <FacebookIcon /> }
+        {props.instagram && <InstagramIcon href={'https://instagram.com/'+props.instagram} /> }
+        {props.dribbble && <DribbbleIcon href={'https://dribbble.com/'+props.dribbble} /> }
+        {props.pinterest && <PinterestIcon /> }
+        {props.soundcloud && <SoundcloudIcon /> }
+        {props.vimeo && <VimeoIcon /> }
+        {props.codepen && <CodepenIcon /> }
+        {props.youtube && <YoutubeIcon /> }
+      </Flex>
+    </Flex>
+  </Container>
+</footer>
